8955d8381ffc5e1b35c058a9a223006b7f277755
Sameri11 <mihailnovoseltsev@MacBook-Air-Mihail.local>
2019-08-19 12:47:10 +0700

    Made RESTful URLS's

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, re_path
-from .views import (CreateUserView, CreateTaskView, UpdateTaskView,
-                    TaskListView, AdminTaskListView, DeleteTaskView,
-                    TasksViewSet)
+from .views import CreateUserView, AdminTaskListView, TasksViewSet
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView
@@ -10,16 +8,9 @@ urlpatterns = [
     path('register/', CreateUserView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('tasks/new/', CreateTaskView.as_view(), name='new_task'),
-    path('tasks/updated/<int:pk>', UpdateTaskView.as_view(), name='update_task'),
-    path('tasks/list/', TaskListView.as_view(), name='task_list'),
-    path('tasks/list/filter/<int:status>/<int:priority>/', 
-         TaskListView.as_view(), name='task_list_filtered'),
-    path('tasks/list/admin/', AdminTaskListView.as_view(), 
+    path('tasks/admin/', AdminTaskListView.as_view(),
          name='admin_task_list'),
-    path('tasks/destroyed/<int:pk>/', DeleteTaskView.as_view(), 
-         name='destroyed_task')
 ]
 router = routers.SimpleRouter()
-router.register(r'task', TasksViewSet, basename='tasks')
+router.register(r'tasks', TasksViewSet, basename='tasks')
 urlpatterns += router.urls
--- api/views.py
+++ api/views.py
@@ -16,52 +16,6 @@ class CreateUserView(CreateAPIView):
     serializer_class = UserSerializer
 
 
-class CreateTaskView(CreateAPIView):
-    model = Tasks
-    serializer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-
-class UpdateTaskView(UpdateAPIView):
-    model = Tasks
-    serializer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return Tasks.objects.filter(pk=self.kwargs['pk'])
-
-    def get_object(self, queryset=None):
-        queryset = self.get_queryset()
-        return get_object_or_404(queryset)
-
-    def partial_update(self, request, *args, **kwargs):
-        kwargs['partial'] = True
-        return self.update(request, *args, **kwargs)
-
-
-class TaskListView(ListAPIView):
-    model = Tasks
-    serializer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        if not self.kwargs:
-            return Tasks.objects.filter(user=self.request.user)
-        elif self.kwargs['status'] and not self.kwargs['priority']:
-            return Tasks.objects.filter(user=self.request.user,
-                                        status=self.kwargs['status'])
-        elif self.kwargs['priority'] and not self.kwargs['status']:
-            return Tasks.objects.filter(user=self.request.user,
-                                        priority=self.kwargs['priority'])
-        elif self.kwargs['status'] and self.kwargs['priority']:
-            return Tasks.objects.filter(user=self.request.user).filter(
-                    status=self.kwargs['status'],
-                    priority=self.kwargs['priority'])
-
-
 class AdminTaskListView(ListAPIView):
     model = Tasks
     serializer_class = TaskSerializer
@@ -69,19 +23,6 @@ class AdminTaskListView(ListAPIView):
 
     def get_queryset(self):
         return Tasks.objects.all()
-
-
-class DeleteTaskView(DestroyAPIView):
-    model = Tasks
-    serializer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return Tasks.objects.filter(pk=self.kwargs['pk'])
-
-    def get_object(self, queryset=None):
-        queryset = self.get_queryset()
-        return get_object_or_404(queryset)
 
 
 class TasksViewSet(viewsets.ModelViewSet):
